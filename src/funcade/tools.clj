053c371e44456b0ec4332f6b8f337e5a37c542f0
anatoly <tolitius@gmail.com>
2020-04-06 08:36:01 -0400

    add decode jwt tools

--- src/funcade/tools.clj
+++ src/funcade/tools.clj
@@ -1,7 +1,19 @@
-(ns funcade.tools)
+(ns funcade.tools
+  (:require [jsonista.core :as json]
+            [clojure.string :as s])
+  (:import [org.apache.commons.codec.binary Base64]))
 
 (defn auth-header
   ([token]
    (auth-header token "Bearer"))
   ([token prefix]
    {"Authorization" (str prefix " " token)}))
+
+(defn decode64 [xs]
+  (-> xs Base64/decodeBase64 String.))
+
+(defn decode-jwt [token]
+  (->> (s/split token #"\.")
+       (take 2) ;; don't decode the signature (for now)
+       (map (comp json/read-value decode64))))
+
