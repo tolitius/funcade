23e2cb3365de47f46cad3805adb2bbf80aa80187
Vigneshwaran Jheyaraman <vigneshwaran_jheyaraman@comcast.com>
2024-06-25 00:29:35 +0530

    support `funcade` to refresh tokens on demand
     ## preface
    With `funcade` middleware to fetch `jwks-keyset` while starting `reitit-middleware` + `buddy-middleware` for certain `authenticator` it
    may require to refresh the token periodically to obtain new `kid` for `jwks` authentication. In order to make sure authentication still
    happens we need to refresh those `keyset` and for which we are exposing functions which actually refreshes the `keyset` and updates the
    refrence for the `variable`
    
     ## fixes done
    * Added `jwks->keys-fn` which returns the `fn` which returns the `cert` given the `kid`
    * Added `refresh-keyset` which actually refreshes the `keyset` and
      updates the `variable-reference`
    * Made the `current-kids` _dynamic_
    * Updated `buddy-middleware` which supports `refresh-keyset?` option
      which actually enables `refresh` support via `clojure-fn`
    * Added `find-kid` function which extracts `kid` from `token`
    * Modified `find-token-key` to make use of `find-kid` function
    * Changes are working fine validated locally
    
     ## unit testing
    ```clojure
    dev=> funcade.jwks/current-kids
    ("kid1", "kid2", "kid3")
    dev=> (funcade.jwks/refresh-kids)
    "[funcade]: done refreshing keyset"
    nil
    dev=> funcade.jwks/current-kids
    ("kid1.1", "kid2.2", "kid3.3")
    ```

--- src/funcade/jwks.clj
+++ src/funcade/jwks.clj
@@ -5,14 +5,10 @@
             [jsonista.core :as j]
             [funcade.tools :as t]))
 
-(defonce ^:private
-  keyset (atom {}))
-
 (defn- group-by-kid [certs]
  (->> (for [{:keys [kid] :as cert} certs]
         [kid (bk/jwk->public-key cert)])
-      (into {})
-      (reset! keyset)))
+      (into {})))
 
 (defn- parse-jwk-response [{:keys [body] :as response}]
   (try
@@ -32,9 +28,43 @@
           (ex-info response)
           throw))))
 
-(defn find-token-key [jwks token]
+(declare refresh-kids
+         find-keyset
+         current-kids)
+
+(defn jwks->keys-fn
+  "Generate the keyset and return getter-fn"
+  [url]
+  (let [keyset    (jwks->keys url)]
+    (alter-var-root (var refresh-kids) (fn [existing-mapping]
+                                         (with-meta
+                                           ;; make sure don't alter the root more than once if already
+                                           ;; set
+                                           (if (-> existing-mapping meta :plugged? true?)
+                                             existing-mapping
+                                             (fn []
+                                               (jwks->keys-fn url)
+                                               (prn "[funcade]: done refreshing keyset")))
+                                           {:plugged? true})))
+    (alter-var-root (var current-kids) (fn [_]
+                                         (with-meta
+                                           (some-> keyset keys)
+                                           (meta (var current-kids)))))
+    (alter-var-root (var find-keyset) (fn [_]
+                                        (with-meta
+                                          (fn [& kids]
+                                            (when (seq kids)
+                                              (get-in keyset kids))) 
+                                          (meta (var find-keyset)))))
+    find-keyset))
+
+(defn find-kid [token]
   (some-> token
           t/decode64
           (json/read-value t/mapper)
-          :kid
+          :kid))
+
+(defn find-token-key [jwks token]
+  (some-> token
+          find-kid  
           jwks))
--- src/funcade/middleware/buddy.clj
+++ src/funcade/middleware/buddy.clj
@@ -1,5 +1,5 @@
 (ns funcade.middleware.buddy
-  (:require [funcade.jwks :as jk]
+  (:require [funcade.jwks :as jwks]
             [buddy.auth :as auth]
             [buddy.auth.protocols :as proto]
             [buddy.sign.jwt :as jwt]
@@ -28,7 +28,7 @@
                 :message (str "access to " (request :uri) " is not authorized")}})))
 
 (defn jwks-backend
-  [{:keys [keyset authfn unauthorized-handler options token-name on-error]
+  [{:keys [keyset authfn unauthorized-handler options token-name on-error keyset-fn]
     :or   {authfn identity token-name "Bearer" options {:alg :rs256}
            on-error #(println "[funcade] error: " %&)}}]
   {:pre [(ifn? authfn)]}
@@ -39,7 +39,10 @@
 
     (-authenticate [_ request data]
       (try
-        (let [tkey (jk/find-token-key keyset data)]
+        (let [tkey (if (and (some? keyset-fn)
+                            (fn? keyset-fn))
+                     (keyset-fn (jwks/find-kid data))
+                     (jwks/find-token-key keyset data))]
           (when-not tkey
             (throw (ex-info "jwt token is signed by unknown key (i.e. no public key in JSON Web Key Sets to verify the signature)"
                             {:type :validation :cause :incorrect-sign-key})))
@@ -61,5 +64,7 @@
 (defn jwks-authenticator
   "JSON Web Key Set specific:
    i.e. needs a 'https://foo.com/bar/jwks' URI that returns unsign keys"
-  [{:keys [uri] :as options}]
-  (jwks-backend (assoc options :keyset (jk/jwks->keys uri))))
+  [{:keys [uri refresh-keyset?] :as options}]
+  (jwks-backend (cond-> options
+                       (not refresh-keyset?)  (assoc :keyset (jwks/jwks->keys uri))
+                       refresh-keyset?        (assoc :keyset-fn (jwks/jwks->keys-fn uri)))))
